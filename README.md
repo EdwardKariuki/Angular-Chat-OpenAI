ee5b9a2cdb1ff36e5040cefc90491a931df1250e
Edward Kariuki <edwardmarathi@gmail.com>
2024-01-27 02:26:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Angular Chat OpenAi
+This repository is created in Angular.
---
+++ src/app/shared/constants/constants.ts
@@ -0,0 +1,22 @@
+export const gptModels = [
+    {
+        'model':'Total number of projects',
+        'description':'The total number of projects in the company to date',
+        'maxRequest':'4,000 tokens'
+    },
+    {
+        'model':'Total number of employees',
+        'description':'The total number of employees in the company to date',
+        'maxRequest':'2,048 tokens'
+    }
+    // {
+    //     'model':'text-babbage-001',
+    //     'description':'Capable of straightforward tasks, very fast, and lower cost.',
+    //     'maxRequest':'2,048 tokens'
+    // },
+    // {
+    //     'model':'text-ada-001',
+    //     'description':'Capable of very simple tasks, usually the fastest model in the GPT-3 series, and lowest cost.',
+    //     'maxRequest':'2,048 tokens'
+    // }
+]
