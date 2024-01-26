5e5c0857b1c610f329a1bbb1527e4cf041f9ec79
Edward Kariuki <edwardmarathi@gmail.com>
2024-01-27 02:33:02 +0300

    Initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-home',
+  templateUrl: './modules/home.component.html',
+  styleUrls: ['./modules/home.component.css'],
+})
+export class HomeComponent implements OnInit {
+
+
+  constructor(
+    
+  ) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  
+}
